700ee43d447eec939a96778ba85a3c904aadc666
MarioUniverseZ <118989308+MarioUniverseZ@users.noreply.github.com>
2022-11-24 22:33:57 +0800

    Rename 專題/FA.R to code and output/FA.R

--- code and output/FA.R
+++ code and output/FA.R
@@ -10,7 +10,7 @@ pj <- read.csv("D:\\project.csv")
 pj <- pj[,-1]
 correlation0 <- cov(pj, use = "pairwise.complete.obs")
 # 
-fa.parallel(correlation0, fa = "fa", main = "�H�۹�")
+fa.parallel(correlation0, fa = "fa", main = "¸H¥Û¹Ï")
 # 
 fa <- fa(correlation0, nfactors = 4, rotate = "varimax", fm = "pa")
 fa0 <- fa(correlation0, nfactors = 4, rotate = "quartimax", fm = "pa")
